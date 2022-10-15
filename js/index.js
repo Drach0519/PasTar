bb48040c00ae4f3619947d96b0b4312b77abc116
Oleksandr Drach <drachsanya051996@gmail.com>
2022-10-15 07:31:29 +0200

    cart counter output

--- js/index.js
+++ js/index.js
@@ -18,3 +18,68 @@ document.getElementById("avatar-3").innerHTML ='<img src="img/bean.jpg"> <div cl
 document.getElementById("avatar-4").innerHTML ='<img src="img/red-lentils-spiral.jpg"> <div class="product-type"><img src="img/spiral.png"></div>';
 document.getElementById("avatar-5").innerHTML ='<img src="img/peas-mushla-yellow.jpg"> <div class="product-type"><img src="img/spiral.png"></div>';
 document.getElementById("avatar-6").innerHTML ='<img src="img/green-lentils-mushla.jpg"> <div class="product-type"><img src="img/spiral.png"></div>';document.getElementById("avatar-7").innerHTML ='<img src="img/peas-green-spiral.jpg"> <div class="product-type"><img src="img/spiral.png"></div>';
+
+
+
+let cart = {
+  product1:[0],
+  product2:[0],
+};
+
+console.log(cart.product1[0])
+document.onclick = event =>{
+  if(event.target.classList.contains('btn-active')){
+    plusFunction(event.target.dataset.id);
+    const inp = document.querySelector('.current-value');
+    const carentValue = +inp.value;
+    let newValue = carentValue + 1;
+    inp.value = newValue;
+    //виводми лічильник
+    if( inp.value >0){
+    document.querySelector('.header-lang .tray span').style.cssText = 'color: #53c6f2';
+    document.querySelector('.current-value').style.cssText = 'opacity: 1';
+  }
+    
+  }
+  if(event.target.classList.contains('btn-minus')){
+    minusFunction(event.target.dataset.id);
+    const inp = document.querySelector('.current-value');
+    const carentValue = +inp.value;
+    let newValue = carentValue - 1;
+    inp.value = newValue;
+    if (inp.value < 0){
+      inp.value =0//перевірка щоб не заходило в мінус 
+      //ховаємо лічильник
+      document.querySelector('.header-lang .tray span').style.cssText = 'color: #666';
+      document.querySelector('.current-value').style.cssText = 'opacity: 0';
+    }
+    //збереження даних
+    localStorage.setItem('text',carentValue)
+    console.log(localStorage.getItem('text'))
+  }
+}
+
+// збільшення товару 
+const plusFunction = id =>{
+  cart[id]++;
+  renderCart();
+
+}
+//уменшение товара
+const minusFunction = id =>{
+  if(cart[id] - 1 == 0){
+    deleteFunction(id);
+    return true; 
+  }
+  cart[id]--;
+  renderCart();
+};
+//удаление товара
+const deleteFunction = id =>{
+  delete cart[id];
+  renderCart();
+}
+const renderCart = () => {
+  console.log(cart);
+
+}
